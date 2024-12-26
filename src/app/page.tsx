5eb4e1917717668282558e063748b53f2c0f5afc
Max <eth@0xma.xyz>
2024-12-26 20:59:44 +0400

    test tgHash serialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ const CreateInvoicePage = () => {
         amount: Number.parseInt(amount),
         unit: currency,
         address: account.bech32Address,
-        tgHash: encodeURIComponent(TgWebApp?.initData ?? ""),
+        tgHash: JSON.stringify(encodeURIComponent(TgWebApp?.initData ?? "")),
       };
       console.log(`invoice is: \n${JSON.stringify(invoice)}`);
 
